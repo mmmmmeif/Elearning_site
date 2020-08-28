932f65d314b1ccd2822e7efd7606348c927645ff
Yuto <yuto@example.com>
2020-08-28 13:51:14 +0900

    add-css-design

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -21,6 +21,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=100)),
                 ('content', models.TextField()),
+                ('answer', models.NullBooleanField()),
                 ('date_posted', models.DateTimeField(default=django.utils.timezone.now)),
                 ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
